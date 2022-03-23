d3e16490231983d92cebc4403b8d93793e8d56e3
CodeSniper <codesniperrrrr@gmail.com>
2022-03-23 22:09:02 +0800

    1. 去除人人开源脚手架的跨域配置 2. 配置网关跨域信息

---
+++ mall_gateway/src/main/java/com/codesniper/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.codesniper.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * 配置跨域
+ *
+ * @author CodeSniper
+ * @since 2022-03-23
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //配置跨域信息
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
